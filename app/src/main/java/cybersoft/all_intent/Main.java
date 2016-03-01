51ede49c779e49ae397503667f627bde2c7010d2
Yorksik <Yorksik@yandex.ru>
2016-03-01 18:38:40 +0300

    test upgrade

--- app/src/main/java/cybersoft/all_intent/Main.java
+++ app/src/main/java/cybersoft/all_intent/Main.java
@@ -1,13 +1,52 @@
 package cybersoft.all_intent;
 
+        import android.content.Intent;
+        import android.net.Uri;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
+Button bCALL;
+Button bMAP;
+Button bWEB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bCALL = (Button) findViewById(R.id.bCALL);
+        bMAP = (Button) findViewById(R.id.bMAP);
+        bWEB = (Button) findViewById(R.id.bWEB);
+
+        bWEB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/") );
+                startActivity(intent);
+            }
+        });
+        bMAP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:20.5525556, 41.5525556"));
+                startActivity(intent);
+            }
+        });
+        bCALL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:12345"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void goToMyBtouser(View view) {
+        Button ToBrous = (Button) findViewById(R.id.ToBrous);
+        Intent intent = new Intent(getApplicationContext(),MyBrous.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/cybersoft/all_intent/MyBrous.java
@@ -0,0 +1,13 @@
+package cybersoft.all_intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyBrous extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_brous);
+    }
+}

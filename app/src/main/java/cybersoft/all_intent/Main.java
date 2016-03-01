0357f8309d57654feef27d839d7927578d9b732f
Yorksik <Yorksik@yandex.ru>
2016-03-01 17:21:20 +0300

    test upgrade

--- app/src/main/java/cybersoft/all_intent/Main.java
+++ app/src/main/java/cybersoft/all_intent/Main.java
@@ -1,7 +1,7 @@
 package cybersoft.all_intent;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
